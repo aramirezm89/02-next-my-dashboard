9054573ad2c7f5103d3d8fbb1016484d11c372ad
aramirezm <aramirez.unimarc@gmail.com>
2025-04-24 21:50:13 -0400

    first commit

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "../components/sidebar/Sidebar";
+import styles from "./dashboard-layout.module.css";
+export default function DashboardPage({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className={styles.root__layout}>
+      <Sidebar />
+      <main className="main container"> {children}</main>
+    </div>
+  );
+}
